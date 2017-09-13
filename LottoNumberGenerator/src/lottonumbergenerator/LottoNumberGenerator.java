e9dbc673ad668074cf7c439dcf5ddb816886c2de
Physika0 <npabon11@gmail.com>
2017-09-13 15:59:59 -0400

    Source file for project

--- LottoNumberGenerator/src/lottonumbergenerator/LottoNumberGenerator.java
+++ LottoNumberGenerator/src/lottonumbergenerator/LottoNumberGenerator.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//********************************************************************************
+// PantherID:  5909334
+// CLASS: COP 2210 – 2017
+// ASSIGNMENT # 1
+// DATE: 9/13/17
+//
+// I hereby swear and affirm that this work is solely my own, and not the work 
+// or the derivative of the work of someone else.
+//********************************************************************************
 package lottonumbergenerator;
 
-/**
- *
- * @author npabon11
- */
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 public class LottoNumberGenerator {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random r = new Random();
+                
+        String greeting = "Here are your winning numbers!";
+        
+        int fantasyFirstNum  = 1 + r.nextInt(36);
+        int fantasySecondNum = 1 + r.nextInt(36);
+        int fantasyThirdNum  = 1 + r.nextInt(36);
+        int fantasyFourthNum = 1 + r.nextInt(36);
+        int fantasyFifthNum  = 1 + r.nextInt(36);
+        String fantasy5 = "Fantasy 5: "
+                + Integer.toString(fantasyFirstNum)  + " " 
+                + Integer.toString(fantasySecondNum) + " " 
+                + Integer.toString(fantasyThirdNum)  + " "
+                + Integer.toString(fantasyFourthNum) + " "
+                + Integer.toString(fantasyFifthNum);
+        
+        int lottoFirstNum  = 1 + r.nextInt(53);
+        int lottoSecondNum = 1 + r.nextInt(53);
+        int lottoThirdNum  = 1 + r.nextInt(53);
+        int lottoFourthNum = 1 + r.nextInt(53);
+        int lottoFifthNum  = 1 + r.nextInt(53);
+        int lottoSixthNum  = 1 + r.nextInt(53);
+        String lotto = "Lotto: "
+                + Integer.toString(lottoFirstNum)  + " "
+                + Integer.toString(lottoSecondNum) + " "
+                + Integer.toString(lottoThirdNum)  + " "
+                + Integer.toString(lottoFourthNum) + " "
+                + Integer.toString(lottoFifthNum)  + " "
+                + Integer.toString(lottoSixthNum);
+                
+        String winningNumbers = greeting + "\n" + fantasy5 + "\n" + lotto;
+        
+        JOptionPane.showMessageDialog(null, "Hello, this program will randomly generate some lotto numbers for you.");
+        JOptionPane.showMessageDialog(null, winningNumbers);
+        
     }
     
 }
